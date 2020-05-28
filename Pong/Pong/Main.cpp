651a0316f2bf32fffb4320adf9d0825798304108
ActuallyTrue <dotaigbe@icloud.com>
2020-05-28 01:19:38 -0500

    SFML has been setup for the project!

--- Pong/Pong/Main.cpp
+++ Pong/Pong/Main.cpp
@@ -1,8 +1,21 @@
-#include <stdio.h>
+#include <SFML/Window.hpp>
 
-int main() {
+int main()
+{
+	sf::Window window(sf::VideoMode(800, 600), "My window");
 
-	printf("Do I work?");
-	while (1);
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+
 	return 0;
 }
